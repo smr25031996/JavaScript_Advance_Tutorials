f0a58737f8a20de97e2a3ab473b5cf53f369db81
Shubham Rajput <rajputshubham615@gmail.com>
2023-10-03 11:24:58 +0530

    practiced input

---
+++ handlingUSerInteractions/pract2.js
@@ -0,0 +1,27 @@
+const button = document.querySelector("button");
+const firstName = document.getElementById("first-name");
+const lastname = document.getElementById("last-name");
+const output = document.getElementById("sidebar");
+const contact = document.getElementById("phone");
+
+
+const email = document.querySelector("#email");
+const myForm = document.querySelector("form");
+
+button.addEventListener("click", ($event) => {
+  event.preventDefault();
+  //   output.textContent = firstName.value + " " + lastname.value;
+  //   output.textContent = email.value;
+  const fullName = document.createElement("h4");
+  const emailId = document.createElement("h4");
+  fullName.textContent = firstName.value + " " + lastname.value;
+  emailId.textContent = email.value;
+  const phone=document.createElement('h4');
+  phone.textContent=contact.value;
+
+  output.appendChild(fullName);
+  output.appendChild(emailId);
+  output.appendChild(phone);
+  console.log(fullName);
+  myForm.reset();
+});
